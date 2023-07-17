b732d5bd483c1bcbd4adf807552f5a7f219c333e
Lucas Oliveira <lucaoxt@gmail.com>
2023-07-17 11:27:37 -0300

    17/07/2023-11:30

--- src/controller/ReportController.ts
+++ src/controller/ReportController.ts
@@ -7,6 +7,8 @@ import { Client } from '../model/Client';
 import { ISaleOrder } from '../entity/SaleOrder';
 import { IFreightOrder } from '../entity/FreightOrder';
 import { ISaleBudget } from '../entity/SaleBudget';
+import { IFreightBudget } from '../entity/FreightBudget';
+import { IBillPay } from '../entity/BillPay';
 
 export class ReportController {
   async clients(req: Request, res: Response) {
@@ -750,6 +752,257 @@ export class ReportController {
     } catch (e) {
       console.error(e);
       return res.status(400).json((e as Error).message);
+    }
+  }
+
+  async freightBudgets(req: Request, res: Response) {
+    if (Object.keys(req.body).length == 0)
+      return res.status(400).json('requisição sem corpo.');
+    const budgets: IFreightBudget[] = req.body.budgets;
+    const filters = req.body.filters;
+
+    let filtersList = '';
+    if (
+      filters.filter == '' &&
+      filters.dateInit == '' &&
+      filters.dateEnd == '' &&
+      filters.client == 0
+    ) {
+      filtersList = 'SEM FILTRAGEM';
+    } else {
+      if (
+        filters.filter != '' &&
+        filters.dateInit != '' &&
+        filters.dateEnd != '' &&
+        filters.client != 0
+      ) {
+        filtersList = `FILTRADO POR FILTRO (${filters.filter}), PERÍODO (${filters.dateInit}) - (${filters.dateEnd}) E CLIENTE (${filters.client})`;
+      } else {
+        if (
+          filters.filter != '' &&
+          filters.dateInit != '' &&
+          filters.dateEnd != '' &&
+          filters.client == 0
+        ) {
+          filtersList = `FILTRADO POR FILTRO (${filters.filter}) E PERÍODO (${filters.dateInit}) - (${filters.dateEnd})`;
+        } else {
+          if (
+            filters.filter != '' &&
+            filters.dateInit == '' &&
+            filters.dateEnd == '' &&
+            filters.client != 0
+          ) {
+            filtersList = `FILTRADO POR FILTRO (${filters.filter}) E CLIENTE (${filters.client})`;
+          } else {
+            if (
+              filters.filter != '' &&
+              filters.dateInit == '' &&
+              filters.dateEnd == '' &&
+              filters.client == 0
+            ) {
+              filtersList = `FILTRADO POR FILTRO (${filters.filter})`;
+            } else {
+              if (
+                filters.filter == '' &&
+                filters.dateInit != '' &&
+                filters.dateEnd != '' &&
+                filters.client != 0
+              ) {
+                filtersList = `FILTRADO POR PERÍODO (${filters.dateInit}) - (${filters.dateEnd}) E CLIENTE (${filters.client})`;
+              } else {
+                if (
+                  filters.filter == '' &&
+                  filters.dateInit != '' &&
+                  filters.dateEnd != '' &&
+                  filters.client == 0
+                ) {
+                  filtersList = `FILTRADO POR PERÍODO (${filters.dateInit}) - (${filters.dateEnd})`;
+                } else {
+                  if (
+                    filters.filter == '' &&
+                    filters.dateInit == '' &&
+                    filters.dateEnd == '' &&
+                    filters.client != 0
+                  ) {
+                    filtersList = `FILTRADO POR FILTRO (${filters.filter}), PERÍODO (${filters.dateInit}) - (${filters.dateEnd}) E CLIENTE (${filters.client})`;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    const runner = AppDataSource.createQueryRunner();
+    try {
+      await runner.connect();
+      const parameterization = (await new Parameterization().findOne(
+        runner,
+      )) as Parameterization;
+      await runner.release();
+      const report = new Report(parameterization);
+      report.ReportTitle('RELATÓRIO DE ORÇAMENTOS DE FRETE', filtersList);
+
+      const col1 = 'CÓD.';
+      const col2 = 'DESCRIÇÃO';
+      const col3 = 'CLIENTE';
+      const col4 = 'DATA';
+      const col5 = 'DESTINO';
+      const col6 = 'AUTOR';
+      const col7 = 'VENCIMENTO';
+      const col8 = 'VALOR (R$)';
+
+      report.SetFont('Arial', 'B', 8);
+      report.SetXY(10, 40);
+      report.Cell(10, 4, col1, 'B');
+      report.Cell(68, 4, col2, 'B');
+      report.Cell(40, 4, col3, 'B');
+      report.Cell(21, 4, col4, 'B');
+      report.Cell(40, 4, col5, 'B');
+      report.Cell(40, 4, col6, 'B');
+      report.Cell(27, 4, col7, 'B');
+      report.Cell(24, 4, col8, 'B');
+
+      let y = 46;
+      report.SetFont('Arial', '', 8);
+      for (const budget of budgets) {
+        const cod = budget.id;
+        const des = budget.description;
+        const cli =
+          budget.client.person.type == 1
+            ? budget.client.person.individual?.name
+            : budget.client.person.enterprise?.fantasyName;
+        const dat = budget.date;
+        const dst = budget.destiny.name + '/' + budget.destiny.state.acronym;
+        const aut = budget.author.person.individual?.name;
+        const val = budget.validate;
+        const vlr = budget.value;
+
+        report.SetXY(10, y);
+        report.Cell(10, 4, cod);
+        report.Cell(68, 4, des);
+        report.Cell(40, 4, cli);
+        report.Cell(21, 4, dat);
+        report.Cell(40, 4, dst);
+        report.Cell(40, 4, aut);
+        report.Cell(27, 4, val);
+        report.Cell(24, 4, vlr);
+
+        y += 6;
+        if (y == 190) {
+          y = 28;
+          report.AddPage();
+        }
+      }
+
+      const fileDate = new Date().toISOString().substring(0, 10);
+      const time = new Date()
+        .toLocaleTimeString('en-US', {
+          timeZone: 'America/Sao_Paulo',
+        })
+        .substring(0, 8);
+
+      report.Output(
+        'F',
+        `reports/RelatorioOrcamentosFrete${fileDate.replaceAll('-', '')}-${time
+          .trim()
+          .replaceAll(':', '')}.pdf`,
+      );
+      return res.json(true);
+    } catch (e) {
+      console.error(e);
+      return res.status(400).json((e as Error).message);
+    }
+  }
+
+  async billsPay(req: Request, res: Response) {
+    if (Object.keys(req.body).length == 0)
+      return res.status(400).json('requisição sem corpo.');
+    const bills: IBillPay[] = req.body.bills;
+    const filters = req.body.filters;
+
+    let filtersList = '';
+    if (
+      filters.filter == '' &&
+      filters.dateInit == '' &&
+      filters.dateEnd == '' &&
+      filters.dueDate == '' &&
+      filters.comission == 0 &&
+      filters.salesman == 0 &&
+      filters.situation == 0
+    ) {
+      filtersList = 'SEM FILTRAGEM';
+    } else {
+      if (
+        filters.filter != '' ||
+        filters.dateInit != '' ||
+        filters.dateEnd != '' ||
+        filters.dueDate != '' ||
+        filters.comission > 0 ||
+        filters.salesman > 0 ||
+        filters.situation != 0
+      ) {
+        filtersList = 'FILTRADO POR';
+
+        if (filters.filter != '') {
+          filtersList += ` FILTRO (${filters.filter})`;
+        }
+
+        if (filters.dateInit != '' && filters.dateEnd != '') {
+          if (filtersList.length > 12) filtersList += ',';
+          filtersList += ` PERÍODO (${filters.dateInit}) - (${filters.dateEnd})`;
+        }
+
+        if (filters.dueDate != '') {
+          if (filtersList.length > 12) filtersList += ',';
+          filtersList += ` VENCIMENTO (${filters.dueDate})`;
+        }
+
+        if (filters.comission > 0) {
+          if (filtersList.length > 12) filtersList += ',';
+          let com = '';
+          switch (filters.comission) {
+            case 1:
+              com = 'SIM';
+              break;
+            case 2:
+              com = 'NÂO';
+              break;
+          }
+          filtersList += ` COMISSÃO (${com})`;
+        }
+
+        if (filters.salesman > 0) {
+          if (filtersList.length > 12) filtersList += ',';
+          filtersList += ` VENDEDOR (${filters.salesman})`;
+        }
+
+        if (filters.situation > 0) {
+          if (filtersList.length > 12) filtersList += ',';
+          let sit = '';
+          switch (filters.situation) {
+            case 1:
+              sit = 'PENDENTE';
+              break;
+            case 2:
+              sit = 'PAGO PARC.';
+              break;
+            case 3:
+              sit = 'PAGO';
+              break;
+          }
+          filtersList += ` SITUAÇÃO (${sit})`;
+        }
+      } else {
+        if (
+          (filters.dateInit != '' && filters.dateEnd == '') ||
+          (filters.dateInit == '' && filters.dateEnd != '')
+        ) {
+          return res
+            .status(400)
+            .json('As datas de inicio e fim devem estar preenchidas.');
+        }
+      }
     }
   }
 }
--- src/routes/report.ts
+++ src/routes/report.ts
@@ -8,5 +8,6 @@ router.post('/clients', userAuthenticated, new ReportController().clients);
 router.post('/sale-orders', userAuthenticated, new ReportController().saleOrders);
 router.post('/freight-orders', userAuthenticated, new ReportController().freightOrders);
 router.post('/sale-budgets', userAuthenticated, new ReportController().saleBudgets);
+router.post('/freight-budgets', userAuthenticated, new ReportController().freightBudgets);
 
 export default router;
